b67bf3a9120b4a96bd9f742b0b38b8ddbe4f2e0b
BarnoMamadzhanova <b@mamadzhanova.com>
2024-05-17 15:09:52 +0200

    getting data from api to console log

--- script/main.js
+++ script/main.js
@@ -1,1 +1,23 @@
 // Sf2FMFg5ZDgB8uu0aboKsldCFJbmgN7h key Giphy API;
+let APIKEY = "Sf2FMFg5ZDgB8uu0aboKsldCFJbmgN7h";
+
+document.addEventListener("DOMContentLoaded", getGifs);
+
+function getGifs() {
+  document.getElementById("btnSearch").addEventListener("click", (e) => {
+    e.preventDefault();
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=`;
+    let str = document.getElementById("search").value.trim();
+    url = url.concat(str);
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((content) => {
+        console.log(content.data);
+        console.log("META", content.meta);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+}
