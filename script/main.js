ebbadc5e216edba19f7b60fa37a0acd7448aa78a
BarnoMamadzhanova <b@mamadzhanova.com>
2024-05-19 16:12:00 +0200

    cleaning style and js files

--- script/main.js
+++ script/main.js
@@ -1,39 +1,39 @@
 // Sf2FMFg5ZDgB8uu0aboKsldCFJbmgN7h key Giphy API;
 
 document.addEventListener("DOMContentLoaded", getGifs);
+const searchBtn = document.getElementById("btnSearch");
+const contentBox = document.querySelector("#content-box");
 
 function getGifs() {
-  document.getElementById("btnSearch").addEventListener("click", (e) => {
+  searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
+
     const APIKEY = "Sf2FMFg5ZDgB8uu0aboKsldCFJbmgN7h";
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=`;
-    let str = document.getElementById("search").value.trim();
-    url = url.concat(str);
-    // console.log(url);
-    fetch(url)
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=24&q=`;
+    const str = document.getElementById("search").value.trim();
+    const urlFetch = url.concat(str);
+
+    fetch(urlFetch)
       .then((response) => response.json())
       .then((content) => {
         // console.log(content.data);
         // console.log("META", content.meta);
 
-        // Clear previous results
-        const contentBox = document.querySelector("#content-box");
         contentBox.innerHTML = "";
 
         for (let gif of content.data) {
-          let figure = document.createElement("figure");
-          let img = document.createElement("img");
+          const figure = document.createElement("figure");
+          const img = document.createElement("img");
           img.src = gif.images.downsized.url;
           img.alt = gif.title;
           figure.appendChild(img);
 
-          let gifFigure = document.createElement("div");
+          const gifFigure = document.createElement("div");
           gifFigure.classList.add("content-box__figure");
           gifFigure.appendChild(figure);
 
           contentBox.appendChild(gifFigure);
         }
-
         document.querySelector("#search").value = "";
       })
       .catch((err) => {
